e9a98ccba0b2ace75a21b9d2e4ec65677a55887b
khaeemm <szdr.1996@gmail.com>
2018-02-17 15:05:58 +0100

    First Tests

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class AccountTest {
+    @Test public void shouldReturnNoTransactionWhenCreated{
+        Account account = new Account();
+
+        String stmt = account.getStatement();
+
+        assertThat(stmt, is("DATE | AMOUNT | BALANCE\n"));
+    }
+
+    @Test public void shouldPrintNoTransactionsWhenCreated{
+        PrintStream printStream = mock(PrintStream.class);
+        Account account = new Account();
+        account.print(printStream);
+        verify(printStream, times(1)).print("DATE | AMOUNT | BALANCE\n");
+    }
+
+    @Test public void shouldReportSingleTransactionWithCorrectFormattingAndBalance() {
+        Account account = new Account();
+        Calendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(2012, Calendar.JANUARY, 10);
+        account.deposit(calendar.getTime(), 100);
+        String report = account.getReport();
+        assertThat(report, equalTo("DATE | AMOUNT | BALANCE\n10/01/2012 | 100.00 | 100.00\n"));
+    }
+
+}
